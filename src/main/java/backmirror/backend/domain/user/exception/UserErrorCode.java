fe3fb4fad174b71345c53289fc74381fdc4d75e3
suhhyun524 <2835223@ewhain.net>
2023-07-20 15:08:53 +0900

    [feat] 유저 에러처리 추가 #2

--- src/main/java/backmirror/backend/domain/user/exception/UserErrorCode.java
+++ src/main/java/backmirror/backend/domain/user/exception/UserErrorCode.java
@@ -12,16 +12,11 @@ import static org.springframework.http.HttpStatus.*;
 @AllArgsConstructor
 public enum UserErrorCode implements BaseErrorCode {
     /* Admin */
-    DUPLICATE_USER(BAD_REQUEST, "USER_400_1", "이미 가입한 어드민입니다."),
-    INVALID_ACTION(BAD_REQUEST, "USER_400_2", "자기 자신에 대한 작업은 수행할 수 없습니다."),
-    NOT_ALLOWED_TO_MODIFY(FORBIDDEN, "USER_403_1", "지원 기간에는 수정할 수 없습니다."),
-    USER_NOT_SIGN_UP(NOT_FOUND, "USER_404_1", "회원으로 가입된 유저가 아닙니다"),
-    USER_NOT_FOUND(NOT_FOUND, "USER_404_2", "존재하지 않는 어드민입니다."),
+    USER_NOT_FOUND(NOT_FOUND, "USER_404_1", "존재하지 않는 어드민입니다."),
 
     /* Data */
-    MISMATCH_CODE(BAD_REQUEST, "USER_400_3", "인증코드가 일치하지 않습니다"),
-    MISMATCH_NEW_PASSWORD(BAD_REQUEST, "USER_400_3", "새비밀번호가 일치하지 않습니다"),
-    MISMATCH_PASSWORD(BAD_REQUEST, "USER_400_4", "비밀번호가 일치하지 않습니다"),
+    MISMATCH_CODE(BAD_REQUEST, "USER_400_1", "인증코드가 일치하지 않습니다"),
+    INVALID_TOKEN(BAD_REQUEST, "USER_400_2", "인증코드가 일치하지 않습니다"),
     DATA_NOT_FOUND(CONFLICT, "USER_404_2", "존재하지 않는 데이터입니다"),
     DUPLICATE_DATA(CONFLICT, "USER_409_1", "이미 존재하는 데이터입니다"),
 
