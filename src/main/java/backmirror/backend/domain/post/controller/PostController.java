e34c4af4dcc3ad5adce2b7debe2452c7fd8c16ae
seoyamin <seoyamin@ewhain.net>
2023-07-20 21:50:39 +0900

    [fix] Swagger 정보 추가 #1

--- src/main/java/backmirror/backend/domain/post/controller/PostController.java
+++ src/main/java/backmirror/backend/domain/post/controller/PostController.java
@@ -7,6 +7,8 @@ import backmirror.backend.domain.post.dto.response.PostListResponseDTO;
 import backmirror.backend.domain.post.service.PostService;
 import backmirror.backend.domain.user.domain.User;
 import backmirror.backend.global.config.user.UserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,10 +17,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/post")
+@Tag(name = "Post")
 public class PostController {
 
     private final PostService postService;
 
+    @Operation(summary = "실패 1건 업로드하기")
     @PostMapping("")
     public ResponseEntity<MessageDTO> savePost(
             @RequestBody PostSaveRequestDTO postSaveRequestDTO,
@@ -27,12 +31,14 @@ public class PostController {
     }
 
 
+    @Operation(summary = "실패 1건 열람하기")
     @GetMapping("/{postId}")
     public ResponseEntity<PostDTO> getPost(@PathVariable Long postId) {
         return ResponseEntity.ok(postService.getPost(postId));
     }
 
 
+    @Operation(summary = "해당 타입의 실패 목록 열람하기")
     @GetMapping("/type/{type}")
     public ResponseEntity<PostListResponseDTO> getPostTypeList(
             @PathVariable String type,
